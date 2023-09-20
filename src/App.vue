ed9e6f90f4d37c29a72ba15acecb207af21a1e2c
Расуль Арсланов <rasularslanoov@gmail.com>
2023-09-20 17:46:28 +0500

    app: Init app, added main component

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <AppMenu />
+</template>
+
+<script>
+import AppMenu from './components/AppMenu.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AppMenu,
+  },
+};
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+
+html {
+  font-size: 62.5%;
+}
+
+body {
+  box-sizing: border-box;
+
+  font-family: 'Roboto', sans-serif;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: inherit;
+}
+</style>
---
+++ src/components/AppMenu.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="app-menu">
+    <div class="app-menu__filters filters">
+      <div class="filters__item filter">
+        <input
+          id="price"
+          type="text"
+          class="filter__input"
+        />
+        <label
+          htmlFor="price"
+          class="filter__label"
+        >
+          Цена
+        </label>
+      </div>
+      <div class="filters__item filter">
+        <input
+          id="price"
+          type="text"
+          class="filter__input"
+        />
+        <label
+          htmlFor="price"
+          class="filter__label"
+        >
+          Кол-во
+        </label>
+      </div>
+      <div class="filters__item filter">
+        <input
+          id="price"
+          type="text"
+          class="filter__input"
+        />
+        <label
+          htmlFor="price"
+          class="filter__label"
+        >
+          Сумма
+        </label>
+      </div>
+      <div class="filters__item filter">
+        <button>Отправить</button>
+        <div>{json}</div>
+      </div>
+    </div>
+    <div class="app-menu__content content">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppMenu',
+  components: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.app-menu {
+  font-size: 1.4rem;
+
+  &__filters.filters {
+    display: flex;
+    gap: 10px;
+
+    .filter {
+      display: flex;
+      flex-direction: column;
+
+      &__input {
+        border: 0.1rem solid #000;
+      }
+
+      &__label {
+
+      }
+    }
+  }
+}
+</style>
